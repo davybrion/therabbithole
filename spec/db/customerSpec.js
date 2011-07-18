c4ce5af1ff423a2e6ccb8c518cbf0c60853f2b47
Davy Brion <ralinx@davybrion.com>
2011-07-18 22:25:40 +0200

    extra test for saving customer plus some refactoring of test code

--- spec/db/customerSpec.js
+++ spec/db/customerSpec.js
@@ -1,43 +1,67 @@
 var mongoose = require('mongoose'),
 	Customer = require('../../lib/entities.js').Customer,
-	CustomerBuilder = require('../builders/customer_builder.js');
+	CustomerBuilder = require('../builders/customer_builder.js'),
+	helper = require('../helper_functions.js');
 
 mongoose.connect('mongodb://localhost/therabbithole_test');
 mongoose.connection.collection('customers').drop();
 
 describe('when a customer is saved', function() {
-	it('should fail with validation errors if required fields are missing', function() {
-		var customer = new Customer();
-		customer.save(function(err) {
-			expect(err).not.toBeNull();
-			expect(err).toHaveRequiredValidationErrorFor('name');
-			expect(err).toHaveRequiredValidationErrorFor('vatNumber');
-			expect(err).toHaveRequiredValidationErrorFor('address.street');
-			expect(err).toHaveRequiredValidationErrorFor('address.postalCode');
-			expect(err).toHaveRequiredValidationErrorFor('address.city');
-			asyncSpecDone();
-		});
-		asyncSpecWait();
-	});
-	
-	it('should not cause errors if all required fields are filled in', function() {
-		var customer = new CustomerBuilder().build();
-		customer.save(function(err) {
-			expect(err).toBeNull();
-			asyncSpecDone();
-		});
-		asyncSpecWait();
-	});
-	
-	it('should contain a default false value for includeContactOnInvoice', function() {
-		var customer = new CustomerBuilder().build();
-		customer.save(function(err) {
-			Customer.findById(customer.id, function(err, result) {
-				expect(result.includeContactOnInvoice).toBe(false);
+	describe('with none of its required fields filled in', function() {
+		it('should fail with validation errors for each required field', function() {
+			var customer = new Customer();
+			customer.save(function(err) {
+				expect(err).not.toBeNull();
+				expect(err).toHaveRequiredValidationErrorFor('name');
+				expect(err).toHaveRequiredValidationErrorFor('vatNumber');
+				expect(err).toHaveRequiredValidationErrorFor('address.street');
+				expect(err).toHaveRequiredValidationErrorFor('address.postalCode');
+				expect(err).toHaveRequiredValidationErrorFor('address.city');
 				asyncSpecDone();
 			});
+			asyncSpecWait();
 		});
-		asyncSpecWait();
+	});
+
+	describe('with all of its required fields filled in', function() {
+		it('should not fail', function() {
+			var customer = new CustomerBuilder().build();
+			customer.save(function(err) {
+				expect(err).toBeNull();
+				asyncSpecDone();
+			});
+			asyncSpecWait();
+		});
+
+		it('should contain a default false value for includeContactOnInvoice', function() {
+			var customer = new CustomerBuilder().build();
+			customer.save(function(err) {
+				Customer.findById(customer.id, function(err, result) {
+					expect(result.includeContactOnInvoice).toBe(false);
+					asyncSpecDone();
+				});
+			});
+			asyncSpecWait();
+		});
+	});
+	
+	describe('with all fields filled in', function() {
+		it('should have the same values when retrieved again', function() {
+			var customer = new CustomerBuilder()
+				.withAddress({ street: 'some street', city: 'some city', postalCode: '1234', country: 'some country' })
+				.withPhoneNumber('123456789')
+				.withContact({ name: 'some name', email: 'some.email@gmail.com' })
+				.withIncludeContactOnInvoice()
+				.build();
+			
+			customer.save(function(err) {
+				Customer.findById(customer.id, function(err, result) {
+					helper.customersShouldBeEqual(result, customer);					
+					asyncSpecDone();
+				});
+			});
+			asyncSpecWait();
+		});
 	});
 });
 
---
+++ spec/helper_functions.js
@@ -0,0 +1,26 @@
+module.exports = {
+	addressesShouldBeEqual: function(address1, address2) {
+		if (address1 === null && address2 === null) { return; }
+		if (address1 !== null) { expect(address2).not.toBeNull(); }
+		if (address2 !== null) { expect(address1).not.toBeNull(); }
+		expect(address1.street).toEqual(address2.street);
+		expect(address1.city).toEqual(address2.city);
+		expect(address1.postalCode).toEqual(address2.postalCode);
+		expect(address1.country).toEqual(address2.country);
+	},
+	contactsShouldBeEqual: function(contact1, contact2) {
+		if (contact1 === null && contact2 === null) { return; }
+		if (contact1 !== null) { expect(contact2).not.toBeNull(); }
+		if (contact2 !== null) { expect(contact1).not.toBeNull(); }
+		expect(contact1.name).toEqual(contact2.name);
+		expect(contact1.email).toEqual(contact2.email);
+	},
+	customersShouldBeEqual: function(customer1, customer2) {
+		expect(customer1.name).toEqual(customer2.name);
+		expect(customer1.vatNumber).toEqual(customer2.vatNumber);
+		expect(customer1.phoneNumber).toEqual(customer2.phoneNumber);
+		expect(customer1.includeContactOnInvoice).toEqual(customer2.includeContactOnInvoice);
+		this.addressesShouldBeEqual(customer1.address, customer2.address);
+		this.contactsShouldBeEqual(customer1.contact, customer2.contact);
+	}
+};
