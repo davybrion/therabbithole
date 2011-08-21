eadeab1e80432a04187d9415b4186774a54c4377
Davy Brion <ralinx@davybrion.com>
2011-08-21 22:58:20 +0200

    updated to node-jslint-all and added exclude path to test.sh so scripts in lib/public would not be checked

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-jslint-all --onevar --regexp --bitwise --newcap --evil false . &&
+node_modules/node-jslint-all/bin/jslint-all.js --onevar --regexp --bitwise --newcap --evil false --exclude=./lib/public . &&
 node_modules/jasmine-node/bin/jasmine-node spec
